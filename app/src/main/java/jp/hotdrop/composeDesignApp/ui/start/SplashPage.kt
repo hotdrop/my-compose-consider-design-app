ecae1df5d596a0c68f2865b6a79003db4ed1d0c1
kenji <hotdrop.tkenji@gmail.com>
2024-06-02 10:38:25 +0900

    SplashPageのTopBarのタイトルが意図したレイアウトになっていなかったため修正

--- app/src/main/java/jp/hotdrop/composeDesignApp/ui/start/SplashPage.kt
+++ app/src/main/java/jp/hotdrop/composeDesignApp/ui/start/SplashPage.kt
@@ -8,14 +8,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -44,8 +47,19 @@ fun SplashPage(
 ) {
     Scaffold(
         topBar = {
-            // TODO 色や配置をStartPageにあわせる
-            TopAppBar(title = { Text(stringResource(id = R.string.splash_title)) })
+            TopAppBar(
+                title = {
+                    Box(
+                        modifier = Modifier.fillMaxWidth(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(stringResource(id = R.string.splash_title), color = Color.White)
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            )
         },
         content = { padding ->
             val state = viewModel.state.collectAsState().value
