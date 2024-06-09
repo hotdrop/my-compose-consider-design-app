4969b78be71597267a3199a9b1907b45cb1f67d6
kenji <hotdrop.tkenji@gmail.com>
2024-06-09 12:20:16 +0900

    不要なimport文を削除

--- app/src/main/java/jp/hotdrop/composeDesignApp/ui/components/CustomTextField.kt
+++ app/src/main/java/jp/hotdrop/composeDesignApp/ui/components/CustomTextField.kt
@@ -1,25 +1,17 @@
 package jp.hotdrop.composeDesignApp.ui.components
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
-import jp.hotdrop.composeDesignApp.R
 
 @Composable
 fun CustomTextField(
